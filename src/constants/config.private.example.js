cd6dedb3d72104480ac100fafd2256fc483f05ef
felix <Felix@felix.com>
2026-06-16 00:58:12 +0800

    config.private.example.js
    (cherry picked from commit 6124af372c551a31ee949f91a56067db65e74d5e)

---
+++ src/constants/config.private.example.js
@@ -0,0 +1,14 @@
+export const PRIVATE_API_ENDPOINTS = {
+    devBaseUrl: '',
+    prodBaseUrl: '',
+    prodFallbackUrls: [
+        '',
+        '',
+    ],
+};
+
+export const REQUEST_SECRETS = {
+    appId: 0,
+    appKey: '',
+    aesKey: '',
+};
